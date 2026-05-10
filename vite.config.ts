35552a1c3e7f0b184c4f826b0b34b6ee7d8b97e0
anya169 <oxina.anya@yandex.ru>
2026-05-11 04:22:37 +0700

    feat: configuring a proxy to resolve the CORS issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://shift-backend.onrender.com/',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path,
+      }
+    }
+  }
 })
